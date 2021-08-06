13dc28aba10a0c4654e824b0efe47a6818997c89
azfar-imtiaz <azfar.imtiaz@live.com>
2021-08-06 18:33:39 +0200

    Create model for meetups

--- meetups/models.py
+++ meetups/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+
+# NOTE: Please ensure that 'meetups' was added to INSTALLED_APPS
+# in settings.py!
+
+class Meetup(models.Model):
+	title = models.CharField(max_length=200)
+	slug = models.SlugField(unique=True)
+	description = models.TextField()
